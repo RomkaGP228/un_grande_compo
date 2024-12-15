a3bf46da8f9f07f16a94674f66f6d37dcfbf1aa8
geo_prepod_pc <maksikbet13@gmail.com>
2024-12-15 23:20:09 +0300

    commit 15.12, main classes added, more description in ts, main run made

---
+++ data/params.py
@@ -0,0 +1,6 @@
+import pygame
+
+window_width = 1280
+window_height = 720
+black = pygame.Color("Black")
+white = pygame.Color("White")
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hi')
+from data.params import *
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Un Grande Compo')
+    window_size = window_width, window_height
+    screen = pygame.display.set_mode(window_size)
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill(black)
+        pygame.display.flip()
+    pygame.quit()
+
+
+
